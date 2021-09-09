c1229f17bead97cb6cda341e3385790b2fc1532d
Kai Pederson <Kai Pederson@DESKTOP-MSADJG6.lan>
2021-09-08 22:54:55 -0500

    fixed naming

--- Test_TLE.m
+++ Test_TLE.m
@@ -7,7 +7,7 @@ AO_73 = satellite(sc, "AO73_TLE", 'Name', 'AO-73')
 gs_la_crosse = groundStation(sc, 43.81810593877638, -91.21248032918966, 'Name', 'La Crosse')
 gs_madison_erb = groundStation(sc, 43.07255162648905, -89.41145475527613, 'Name', 'ERB')
 
-tiny_tim = [];
+tx_opps = [];
 
 for hr = 0:23
     for min = 0:59 
@@ -15,9 +15,9 @@ for hr = 0:23
             time = datetime(2021,7,15,hr,min,sec);
             pos_AO_73 = states(AO_73(1),time,"CoordinateFrame","geographic");
 
-            tim = satcom.internal.linkbudgetApp.computeElevation(43.81810593877638, -91.21248032918966, 0, pos_AO_73(1), pos_AO_73(2), pos_AO_73(3));
-            if tim > 25
-                tiny_tim = [tiny_tim, tim, string(time)];
+            angle = satcom.internal.linkbudgetApp.computeElevation(43.81810593877638, -91.21248032918966, 0, pos_AO_73(1), pos_AO_73(2), pos_AO_73(3));
+            if angle > 25
+                tx_opps = [tx_opps, angle, string(time)];
             end
         end
     end
