dd736a4d87ef3056c945d0b17296ccf058e92419
Kai Pederson <Kai Pederson@DESKTOP-MSADJG6.lan>
2021-09-15 22:46:19 -0500

    fixed naming and added tles for experiment

--- demo_9SEP21.m
+++ demo_9SEP21.m
@@ -8,6 +8,7 @@ JO_97 = satellite(sc, "JO97_TLE", 'Name', 'JO-97')
 AO_73 = satellite(sc, "AO73_TLE", 'Name', 'AO-73')
 gs_la_crosse = groundStation(sc, 43.81810593877638, -91.21248032918966, 'Name', 'La Crosse')
 gs_madison_erb = groundStation(sc, 43.07255162648905, -89.41145475527613, 'Name', 'ERB')
+
 % time = datetime(2021,7,15,11,18,23)
 % pos_AO_73 = states(AO_73(1),time,"CoordinateFrame","geographic")
 % time = datetime(2021,7,15,17,24,42)
