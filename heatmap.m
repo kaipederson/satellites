96468abe4891a7f644475f571c2744412ca79745
kaipederson <kpederson6161@gmail.com>
2021-12-09 11:44:51 -0600

    added some comments for clarity in heatmap

--- heatmap.m
+++ heatmap.m
@@ -1,8 +1,12 @@
-lat = -89.4012;
-long = 47.0731;
+%enter GPS for TARGET location below
+lat = -121.8947;
+long = 36.6002;
+
+%constants for lat/long arithmetic
 fifty_km_minutes = 0.4522;
 fifty_km_minutes_angle = 0.31975;
 
+%arrays to store "neighboring" locations relative to target
 n_neighbors = ones(2,16);
 s_neighbors = ones(2,16);
 w_neighbors = ones(2,16);
@@ -33,6 +37,8 @@ end
 
 neighbors = [n_neighbors; s_neighbors; w_neighbors; e_neighbors; nw_neighbors; sw_neighbors; ne_neighbors; se_neighbors];
 
+%below should be left commented unless experimenting with map parameters
+%following completion of script
 
 % l = [n_neighbors, s_neighbors, w_neighbors, e_neighbors, nw_neighbors, sw_neighbors, ne_neighbors, se_neighbors];
 % l = l';
@@ -44,15 +50,15 @@ neighbors = [n_neighbors; s_neighbors; w_neighbors; e_neighbors; nw_neighbors; s
 %     for j = 1:16
 %         times(index) = results(i, j);
 %         if times(index) >= 60
-%             colors(index) = 'ro';
+%             colors(index) = 'r*';
 %         elseif times(index) >= 45
-%             colors(index) = 'yo';
+%             colors(index) = 'y*';
 %         elseif times(index) >= 30
-%             colors(index) = 'go';
+%             colors(index) = 'g*';
 %         elseif times(index) >= 15
-%             colors(index) = 'co';
+%             colors(index) = 'b*';
 %         else
-%             colors(index) = 'o';
+%             colors(index) = '*';
 %         end
 %         index = index + 1;
 %     end
@@ -61,7 +67,7 @@ neighbors = [n_neighbors; s_neighbors; w_neighbors; e_neighbors; nw_neighbors; s
 % locations = table([l(:,1)],[l(:,2)],times, colors);
 % locations.Properties.VariableNames = {'Lat' 'Long' 'TX Duration' 'Color'}
 % 
-% geobasemap grayterrain;
+% geobasemap colorterrain;
 % MapCenterMode = 'auto'
 % for points = 1:128
 %     hold on
@@ -69,34 +75,34 @@ neighbors = [n_neighbors; s_neighbors; w_neighbors; e_neighbors; nw_neighbors; s
 %     hold off
 % end
 
-
 results = ones(16,16);
-
+%informative use only
 location = ["north", "north", "south", "south", "west", "west", "east", "east","northwest", "northwest", "southwest", "southwest", "northeast", "northeast", "southeast", "southeast"];
 
 for direction = 1:2:16
     "Begin " + location(direction) + ":"
     for points = 1:16
+        %sets duration in which windows are calculated, make sure TLE is
+        %updated for date(s) of interest
         startTime = datetime(2021,9,14,0,0,0);
         stopTime = startTime + days(1);
+        %granularity of simulation
         sampleTime = 15;
         sc = satelliteScenario(startTime, stopTime, sampleTime);
+        %tle file is specified as second arg below
         sat = satellite(sc, "constellation_curr");
         
-        for idx = 1:numel(sat)
-            name = sat(idx).Name;
-            conicalSensor(sat(idx),"Name",name,"MaxViewAngle",130);
-        end
-        
         name = "Center";
+        %specifies minimum angle of TX to satellite
         minElevationAngle = 25; % degrees
         geoSite = groundStation(sc, long, lat, "Name", name, "MinElevationAngle",minElevationAngle);
         geoSite2 = groundStation(sc, neighbors(direction, points), neighbors(direction + 1, points), "Name", "Neighbor", "MinElevationAngle",minElevationAngle);
-        %geoplot(neighbors(direction, points), neighbors(direction + 1, points),'r-*')
+        %check accesses for each satellite in constellation (specified by
+        %TLE file
         for idx = 1:numel(sat)
             ac(idx) = access(geoSite2, sat(idx), geoSite);
         end
-        %accessIntervals(ac)
+        a = accessIntervals(ac)
         for idx = 1:numel(ac)
             [s,time] = accessStatus(ac(idx));
             
@@ -119,13 +125,15 @@ for direction = 1:2:16
 
     end
 end
-    xlswrite(['Madison.xlsx'],results)
 
+xlswrite(['Madison.xlsx'],results)
+%some matrix manipulation to populate table for mapping
 l = [n_neighbors, s_neighbors, w_neighbors, e_neighbors, nw_neighbors, sw_neighbors, ne_neighbors, se_neighbors];
 l = l';
 times = ones(128, 1);
 colors = strings(128,1);
 
+%assigns colors and symbols to locations based on TX time
 index = 1;
 for i = 1:2:16
     for j = 1:16
@@ -148,7 +156,10 @@ end
 locations = table([l(:,1)],[l(:,2)],times, colors);
 locations.Properties.VariableNames = {'Lat' 'Long' 'TX Duration' 'Color'}
 
+%specify map type here, see documentation for options
 geobasemap grayterrain;
+%if unspecified, map will fit to points plotted by script, this sets the
+%view to the continental US for context
 geolimits([20 60],[-155 -60])
 for points = 1:128
     hold on
